f143bdfd7a8c768c2c8aac9011eca561d9563f1d
soul-mortal <yodgorsalohiddinov44@gmail.com>
2025-03-24 19:43:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const name = prompt("Ismingizni kiriting:");
+const spaceId = prompt("Space ID ni kiriting:");
+const spacePassword = prompt("Space Password ni kiriting:");
+
+const userInfo = {
+    name: name,
+    id: spaceId,
+    password: spacePassword
+};
+
+console.log(userInfo);
